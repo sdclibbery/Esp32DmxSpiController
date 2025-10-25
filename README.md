c3e7fd7f3dae22afe850b36b264d5661af1a7e03
Steve <sdclibbery@users.noreply.github.com>
2025-10-25 23:43:13 +0100

    fix stuff

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Esp32DmxSpiController
 ESP32 based DMX controller for SPI LED strips
+
+## Libraries
+FastLED, SparkFunDMX
+
+## DMX Channel Mapping
+1: Mode
+2: Control
+3: Smoothing
+4-6: Back RGB
+7-9: Fore RGB
+
+## Modes
+0: 
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,60 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <SparkFunDMX.h>
+#include <FastLED.h>
 
+/* TODO
+ Get it to compile
+ First actual mode
+ DMX base channel
+*/
+
+// Hardware Definitions for ESP32 DMX Shield (UART2)
+// The SparkFun DMX Shield typically uses UART2 on the ESP32.
+#define DMX_UART_NUM  2
+#define DMX_RX_PIN    16 // UART2 Receive Pin
+#define DMX_TX_PIN    17 // UART2 Transmit Pin
+#define DMX_EN_PIN    21 // Direction Enable Pin (Controls RS-485 transceiver)
+
+// DMX 
+HardwareSerial dmxSerial(DMX_UART_NUM);
+SparkFunDMX dmx;
+
+// Channel Defintions
+#define TOTAL_CHANNELS 9
+const uint16_t dmxStartChannel = 1;
+
+// FastLED Pin Definitions for ESP32 WROOM
+#define CLOCK 5
+#define DATA0 19
+#define DATA1 18
+#define DATA2 27
+
+// LED setup
+#define NUM_LEDS 30
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  delay( 500 ); // power up delay
+  Serial.println("Setup starting.");
+
+  Serial.begin(115200);
+  dmxSerial.begin(250000, SERIAL_8N2, DMX_RX_PIN, DMX_TX_PIN);
+  dmx.begin(dmxSerial, DMX_EN_PIN, 512); 
+  dmx.setComDir(DMX_READ_DIR);
+
+  FastLED.addLeds<WS2812B, DATA0, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness( 64 );
+
+  Serial.println("Setup complete.");
 }
 
+unsigned char t = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while(dmx.dataAvailable() == false) {
+      dmx.update();
+  }
+  dmx.readByte(dmxStartChannel);
+  
+  leds[0].r = t++;
+  FastLED.show();
 }
