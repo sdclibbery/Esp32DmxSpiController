9931a479b3472f96b0b42ad2cd3c7c6470029328
Steve <sdclibbery@users.noreply.github.com>
2025-10-28 19:04:20 +0000

    Swap FastLed to NexPixelBus

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ESP32 based DMX controller for SPI LED strips
 
 ## Libraries
-FastLED, SparkFunDMX
+NeoPixelBus, SparkFunDMX
 
 ## DMX Channel Mapping
 1: Mode
@@ -12,9 +12,31 @@ FastLED, SparkFunDMX
 7-9: Fore RGB
 
 ## Modes
-0: Solid: Blend entire strip between back and fore based on control, smoothing does nothing
-1: VU: fore colour rises from start of strip, control is length of fore section, smoothing is length of lerp at end of section
-2: VU: fore colour falls from end of strip, control is length of fore section, smoothing is length of lerp at end of section
-3: VU: fore colour expands from centre of strip, control is length of fore section, smoothing is length of lerp at ends of section
-blocks, flow, fizzle, race, hsv blended VU modes allowing rainbow?
+### Palettes
+There are 4 variations of each mode with the following palettes:
+0: BF: lerp from back colour to fore colour
+1: OBF: lerp from off (black) to back to fore
+2: BFO: lerp from back to fore to off (black)
+3: BOF: lerp from back to off (black) to fore
+
+### Solid
+0: Solid: Blend entire strip through the palette based on control, smoothing does nothing
+Blocks: Alternating blocks betwen back and fore. Control is block size, Smoothing is lerp size
+
+### Gradient
+StartGradient: fore colour rises from start of strip, control is length of fore section, smoothing is lerp size
+EndGradient: fore colour falls from end of strip, control is length of fore section, smoothing is lerp size
+MidGradient: fore colour expands from centre of strip, control is length of fore section, smoothing is lerp size
+Paddle: One light is on with fore colour. Control is location. Fades through back colour when it moves. Smoothing is trail fade rate
+
+### Drawing
+Draw: Control sets draw pos. Fore is drawn into the strip at draw pos. Smoothing is scroll pos.
+       ????how does scroll work when channel wraps a byte?
+Fade: Same as Draw, but drawn pixels slowly fade back to back colour
+
+### Noise
+Noise: Perlin noise between fore and back. Control is seed. Smoothing is scale (or octaves??).
+Noise2: Same as Noise, but noise is between off/back/fore
+Flame: Back/Fore/off noise from bottom of strip. Control is height. Smoothing is speed.
+        ??Can this be a gradient variation?? Have gradients from back/fore/off not just back/fore??
 
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <SparkFunDMX.h>
-#include <FastLED.h>
+#include <NeoPixelBus.h>
 
 /* TODO
- First actual mode
+ First actual mode: 0: Solid: Blend entire strip between back and fore based on control, smoothing does nothing
+  0: Solid: Blend entire strip through the palette based on control, smoothing does nothing
+  4 palettes from mode
+  function for actual mode; pass palette fn in as fn ptr
  Consider rewriting Serial handling: https://gemini.google.com/app/9bacdb891b977834 (but Geminis code is all over the place, inc setTimeout taking MILLIs)
  DMX base channel
 */
@@ -18,43 +21,77 @@
 // DMX 
 HardwareSerial dmxSerial(DMX_UART_NUM);
 SparkFunDMX dmx;
-
-// Channel Defintions
-#define TOTAL_CHANNELS 9
+#define DMX_MODE      0
+#define DMX_CONTROL   1
+#define DMX_SMOOTH    2
+#define DMX_BACK_R    3
+#define DMX_BACK_G    4
+#define DMX_BACK_B    5
+#define DMX_FORE_R    6
+#define DMX_FORE_G    7
+#define DMX_FORE_B    8
+#define DMX_CHANNELS  9
 const uint16_t dmxStartChannel = 1;
+uint8_t dmxData[DMX_CHANNELS] = {0};
 
-// FastLED Pin Definitions for ESP32 WROOM
+// LED setup
+// Pin Definitions
 #define CLOCK 5
 #define DATA0 19
 #define DATA1 18
-#define DATA2 27
+#define DATA2 27 
+const uint16_t pixelCount = 4;
+typedef NeoPixelBus<NeoGrbFeature, NeoEsp32I2s1X8Ws2812xMethod> PixelStrip;
+// typedef NeoPixelBus<NeoGrbwFeature, NeoEsp32I2s1X8Sk6812Method> PixelStrip;
+PixelStrip strip1(pixelCount, DATA0);
 
-// LED setup
-#define NUM_LEDS 30
-CRGB leds[NUM_LEDS];
+struct FixtureData {
+  uint8_t mode;
+  float control;
+  float smooth;
+  RgbColor fore;
+  RgbColor back;
+};
 
 void setup() {
-  delay( 500 ); // power up delay
+  Serial.begin(115200);
+  while (!Serial); // wait for serial attach
   Serial.println("Setup starting.");
 
-  Serial.begin(115200);
   dmxSerial.begin(250000, SERIAL_8N2, DMX_RX_PIN, DMX_TX_PIN);
   dmx.begin(dmxSerial, DMX_EN_PIN, 512); 
   dmx.setComDir(DMX_READ_DIR);
 
-  FastLED.addLeds<WS2812B, DATA0, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness( 64 );
+  strip1.Begin(); strip1.Show(); // Clear strip
 
   Serial.println("Setup complete.");
 }
 
-unsigned char t = 0;
+RgbColor paletteBF(const RgbColor& back, const RgbColor& fore, float lerp) {
+  return RgbColor::LinearBlend(back, fore, lerp);
+}
+
+void modeSolid(const FixtureData& data, PixelStrip& strip) {
+  for (uint16_t i=0; i<strip.PixelCount(); i++ ) {
+    strip.SetPixelColor(i, paletteBF(data.back, data.fore, data.control));
+  }
+}
+
+uint8_t t = 0;
 void loop() {
+  delay(100);
+
   while(dmx.dataAvailable() == false) {
       dmx.update();
   }
-  dmx.readByte(dmxStartChannel);
-  
-  leds[0].r = t++;
-  FastLED.show();
+  dmx.readBytes(dmxData, DMX_CHANNELS, dmxStartChannel);
+  FixtureData fixtureData;
+  fixtureData.mode = dmxData[DMX_MODE];
+  fixtureData.control = ((float)dmxData[DMX_CONTROL])/255;
+  fixtureData.smooth = ((float)dmxData[DMX_SMOOTH])/255;
+  fixtureData.fore = RgbColor(dmxData[DMX_FORE_R],dmxData[DMX_FORE_G],dmxData[DMX_FORE_B]);
+  fixtureData.back = RgbColor(dmxData[DMX_BACK_R],dmxData[DMX_BACK_G],dmxData[DMX_BACK_B]);
+
+  modeSolid(fixtureData, strip1);
+  strip1.Show();
 }
